2b0b8a25a598797fbdae91aa4a08be27bd975ea3
suvamdawn94 <suvamdawn94@gmail.com>
2018-12-30 21:29:10 +0530

    add theme

--- app/src/main/java/com/suvamdawn/advance/google/map/MainActivity.java
+++ app/src/main/java/com/suvamdawn/advance/google/map/MainActivity.java
@@ -1,8 +1,12 @@
 package com.suvamdawn.advance.google.map;
 
+import android.content.res.Resources;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,34 +16,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
+import static android.support.constraint.Constraints.TAG;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener,GoogleMap.OnCameraMoveStartedListener {
+    private String theme="standard";
+    SupportMapFragment mapView;
+    GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        /*Map View*/
+        mapView = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapView);
+        mapView.getMapAsync(this);
+        setTitle("Standard");
     }
 
     @Override
@@ -51,25 +59,20 @@ public class MainActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -80,22 +83,104 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.standard) {
+            if(mMap!=null){
+                mMap.clear();
+                theme="standard";
+                setTitle("Standard");
+                mapView.getMapAsync(this);
+            }
+        } else if (id == R.id.silver) {
+            if(mMap!=null){
+                mMap.clear();
+                theme="silver";
+                setTitle("Silver");
+                mapView.getMapAsync(this);
+            }
+        } else if (id == R.id.retro) {
+            if(mMap!=null){
+                mMap.clear();
+                theme="retro";
+                setTitle("Retro");
+                mapView.getMapAsync(this);
+            }
+        } else if (id == R.id.dark) {
+            if(mMap!=null){
+                mMap.clear();
+                theme="dark";
+                setTitle("Dark");
+                mapView.getMapAsync(this);
+            }
+        } else if (id == R.id.night) {
+            if(mMap!=null){
+                mMap.clear();
+                theme="night";
+                setTitle("Night");
+                mapView.getMapAsync(this);
+            }
+        } else if (id == R.id.aubergine) {
+            if(mMap!=null){
+                mMap.clear();
+                theme="aubergine";
+                setTitle("Aubergine");
+                mapView.getMapAsync(this);
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onCameraMoveStarted(int i) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        try {
+            mMap=googleMap;
+            boolean success=false;
+            if(theme=="standard"){
+                success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this, R.raw.style_json_standard));
+            }else if(theme=="silver"){
+                success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this, R.raw.style_json_silver));
+            }else if(theme=="retro"){
+                success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this, R.raw.style_json_retro));
+            }else if(theme=="dark"){
+                success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this, R.raw.style_json_dark));
+            }else if(theme=="night"){
+                success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this, R.raw.style_json_night));
+            }else if(theme=="aubergine"){
+                success = mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MainActivity.this, R.raw.style_json_aubergine));
+            }
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(12.9716, 77.5946),14));
     }
 }
